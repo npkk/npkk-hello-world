cb659ac66aaafb47c0468fd2c93a335c9aafd296
npkk <kob4.nemu@gmail.com>
2026-03-08 17:06:50 +0900

    implement tray

--- tauri/tray/src-tauri/src/lib.rs
+++ tauri/tray/src-tauri/src/lib.rs
@@ -1,3 +1,9 @@
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    Manager, RunEvent, WindowEvent,
+};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,8 +13,66 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let show_item = MenuItem::with_id(app, "show", "Show", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_item, &show_item])?;
+            let _tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "quit" => {
+                        println!("quit selected");
+                        app.exit(0);
+                    }
+                    "show" => {
+                        println!("show selected");
+                        if let Some(window) = app.get_webview_window("main") {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    }
+                    _ => {
+                        println!("menu {:?} selected", event.id)
+                    }
+                })
+                .on_tray_icon_event(|tray, event| {
+                    // 左クリックでウィンドウを表示
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    }
+                })
+                .build(app)?;
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        // .run(tauri::generate_context!())から.build(tauri::generate_context!())にしてRunEventをrunで拾えるようにする
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|app, event| {
+            // 閉じるボタンでトレイに引っ込む
+            if let RunEvent::WindowEvent {
+                label,
+                event: WindowEvent::CloseRequested { api, .. },
+                ..
+            } = event
+            {
+                if label == "main" {
+                    if let Some(window) = app.get_webview_window(&label) {
+                        window.hide().unwrap();
+                    }
+                    api.prevent_close();
+                }
+            }
+        });
 }
