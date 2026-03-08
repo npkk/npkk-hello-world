17ac0805cd8d56307e1d53b064bf9250d0f0526e
npkk <kob4.nemu@gmail.com>
2026-03-08 19:19:21 +0900

    [tauri/db] biome fix

--- tauri/db/src/App.tsx
+++ tauri/db/src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import { invoke } from '@tauri-apps/api/core';
+import { useId, useState } from 'react';
+
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
+  const greetFormId = useId();
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }));
   }
 
   return (
@@ -17,13 +19,13 @@ function App() {
       <h1>Welcome to Tauri + React</h1>
 
       <div className="row">
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noopener">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
-        <a href="https://tauri.app" target="_blank">
+        <a href="https://tauri.app" target="_blank" rel="noopener">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -37,7 +39,7 @@ function App() {
         }}
       >
         <input
-          id="greet-input"
+          id={greetFormId}
           onChange={(e) => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
